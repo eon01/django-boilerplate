98762d2bb9f69ad4070849841c4949fcdbac9334
Aymen EL AMRI <aymen@eralabs.io>
2024-02-28 19:30:28 +0100

    update db dockerfile

--- db/scripts/init-user-permissions.sh
+++ db/scripts/init-user-permissions.sh
@@ -6,14 +6,14 @@ set -euxo pipefail
     echo "Starting script execution..."
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     \x
-        CREATE DATABASE skriptshift;
-        CREATE USER skriptshift WITH PASSWORD '$POSTGRES_PASSWORD';
-        ALTER ROLE skriptshift SET client_encoding TO 'utf8';
-        ALTER ROLE skriptshift SET default_transaction_isolation TO 'read committed';
-        ALTER ROLE skriptshift SET timezone TO 'UTC';
-        GRANT ALL PRIVILEGES ON DATABASE skriptshift TO skriptshift;
-        GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO skriptshift;
-        GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO skriptshift;
-        ALTER USER skriptshift CREATEDB;
+        CREATE DATABASE boilerplate;
+        CREATE USER boilerplate WITH PASSWORD '$POSTGRES_PASSWORD';
+        ALTER ROLE boilerplate SET client_encoding TO 'utf8';
+        ALTER ROLE boilerplate SET default_transaction_isolation TO 'read committed';
+        ALTER ROLE boilerplate SET timezone TO 'UTC';
+        GRANT ALL PRIVILEGES ON DATABASE boilerplate TO boilerplate;
+        GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO boilerplate;
+        GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO boilerplate;
+        ALTER USER boilerplate CREATEDB;
     EOSQL
 } &> /var/log/init-user-permissions.log
