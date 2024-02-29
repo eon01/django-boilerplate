b8666d03b5cbdb47d88a7fbef780c72d4db56f63
Aymen EL AMRI <aymen@eralabs.io>
2024-02-29 12:51:45 +0100

    fix psql script

--- db/scripts/init-user-permissions.sh
+++ db/scripts/init-user-permissions.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 set -euxo pipefail
 
-
 {
     echo "Starting script execution..."
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOF
     \x
-        CREATE DATABASE boilerplate;
-        CREATE USER boilerplate WITH PASSWORD '$POSTGRES_PASSWORD';
-        ALTER ROLE boilerplate SET client_encoding TO 'utf8';
-        ALTER ROLE boilerplate SET default_transaction_isolation TO 'read committed';
-        ALTER ROLE boilerplate SET timezone TO 'UTC';
-        GRANT ALL PRIVILEGES ON DATABASE boilerplate TO boilerplate;
-        GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO boilerplate;
-        GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO boilerplate;
-        ALTER USER boilerplate CREATEDB;
-    EOF
+    CREATE DATABASE proj;
+    CREATE USER proj WITH PASSWORD '$POSTGRES_PASSWORD';
+    ALTER ROLE proj SET client_encoding TO 'utf8';
+    ALTER ROLE proj SET default_transaction_isolation TO 'read committed';
+    ALTER ROLE proj SET timezone TO 'UTC';
+    GRANT ALL PRIVILEGES ON DATABASE proj TO proj;
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO proj;
+    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO proj;
+    ALTER USER proj CREATEDB;
+EOF
 } &> /var/log/init-user-permissions.log
